fd1b4edc2225efb5b8a9108b2ad3518644aaffa5
supzlo <khuehuyen4@gmail.com>
2023-12-23 11:45:57 -0600

    use pep8 style names

--- src/refact/statement.py
+++ src/refact/statement.py
@@ -2,8 +2,8 @@ import locale
 
 
 def statement(invoice: dict, plays: dict):
-    totalAmount = 0
-    volumeCredits = 0
+    total_amount = 0
+    volume_credits = 0
     result = f"Statement for {invoice['customer']}\n"
 
     def format(amount):
@@ -12,33 +12,33 @@ def statement(invoice: dict, plays: dict):
 
     for perf in invoice["performances"]:
         play = plays[perf["playID"]]
-        thisAmount = 0
+        this_amount = 0
 
         match play["type"]:
             case "tragedy":
-                thisAmount = 40000
+                this_amount = 40000
                 if perf["audience"] > 30:
-                    thisAmount += 1000 * (perf["audience"] - 30)
+                    this_amount += 1000 * (perf["audience"] - 30)
             case "comedy":
-                thisAmount = 30000
+                this_amount = 30000
                 if perf["audience"] > 20:
-                    thisAmount += 10000 + 500 * (perf["audience"] - 20)
-                thisAmount += 300 * perf["audience"]
+                    this_amount += 10000 + 500 * (perf["audience"] - 20)
+                this_amount += 300 * perf["audience"]
             case _:
                 raise Exception(f"unknown type: ${play['type']}")
 
         # add volume credits
-        volumeCredits += max(perf["audience"] - 30, 0)
+        volume_credits += max(perf["audience"] - 30, 0)
         # add extra credit for every ten comedy attendees
         if "comedy" == play["type"]:
-            volumeCredits += perf["audience"] // 5
+            volume_credits += perf["audience"] // 5
 
         # print line for this order
         result += (
-            f"  {play['name']}: {format(thisAmount/100)} ({perf['audience']} seats)\n"
+            f"  {play['name']}: {format(this_amount/100)} ({perf['audience']} seats)\n"
         )
-        totalAmount += thisAmount
+        total_amount += this_amount
 
-    result += f"Amount owed is {format(totalAmount/100)}\n"
-    result += f"You earned {volumeCredits} credits\n"
+    result += f"Amount owed is {format(total_amount/100)}\n"
+    result += f"You earned {volume_credits} credits\n"
     return result
